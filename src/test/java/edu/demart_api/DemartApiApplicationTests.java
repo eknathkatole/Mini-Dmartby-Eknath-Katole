50afb27451ea6fe013677a3e303b0b2ecc789976
Eknath U. Katole <katoleeknat7@gmail.com>
2026-08-23 00:35:31 +0530

    Clean backend repo and fix H2 test profile for GitHub Actions

--- src/test/java/edu/demart_api/DemartApiApplicationTests.java
+++ src/test/java/edu/demart_api/DemartApiApplicationTests.java
@@ -2,8 +2,10 @@ package edu.demart_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DemartApiApplicationTests {
 
 	@Test
